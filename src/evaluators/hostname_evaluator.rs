dc2c566b51181564a788805ce707e3420c752564
Jacob Rodal <jrodal@meta.com>
2025-07-02 09:49:53 -0700

    refactor

--- src/evaluators/hostname_evaluator.rs
+++ src/evaluators/hostname_evaluator.rs
@@ -2,7 +2,7 @@ use anyhow::Context;
 use anyhow::Result;
 use serde::Deserialize;
 use serde::Serialize;
-
+use std::ffi::OsString;
 use super::EvaluatorTrait;
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -10,20 +10,27 @@ pub struct HostnameEvaluator {
     target: String,
 }
 
+// this is kind of stupid, but I don't want to deal with mocking right now
+impl HostnameEvaluator {
+    #[cfg(not(test))]
+    fn get_hostname() -> Result<OsString> {
+        hostname::get().context("Failed to get hostname")
+    }
+
+    #[cfg(test)]
+    fn get_hostname() -> Result<OsString> {
+        Ok("test-hostname".into())    
+    }
+}
+
 impl EvaluatorTrait for HostnameEvaluator {
     fn evaluate(&self) -> Result<bool> {
-        let hostname = if cfg!(test) {
-            "test-hostname".into()
-        } else {
-            hostname::get().context("Failed to get hostname")?
-        };
-
+        let hostname = Self::get_hostname()?;
         let hostname_str = hostname
             .to_str()
             .context("Failed to convert hostname to string")?;
         Ok(self.target == hostname_str)
     }
-
 }
 
 
