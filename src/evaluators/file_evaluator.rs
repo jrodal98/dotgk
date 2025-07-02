c5b51a0bf15328631d726bd1e90667befcb2c557
Jacob Rodal <jrodal@meta.com>
2025-07-02 09:58:27 -0700

    file evaluator tests

--- src/evaluators/file_evaluator.rs
+++ src/evaluators/file_evaluator.rs
@@ -16,3 +16,42 @@ impl EvaluatorTrait for FileEvaluator {
         Ok(PathBuf::from(&self.path).exists())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::gatekeeper::Gatekeeper;
+    use anyhow::Result;
+
+    fn get_gk(target: &str) -> Result<Gatekeeper> {
+        let gk_json = serde_json::json!({
+            "groups": [
+                {
+                    "type": "file",
+                    "args": {
+                        "path": target
+                    },
+                    "condition": "eq"
+                }
+            ]
+        }).to_string();
+        Gatekeeper::from_json(&gk_json)
+    }
+
+    fn helper(target: &str, expected: bool) -> Result<()> {
+        let gk = get_gk(target)?;
+        let result = gk.evaluate()?;
+        assert_eq!(result, expected);
+        Ok(())
+    }
+
+
+    #[test]
+    fn test_pass() -> Result<()> {
+        helper("src/evaluators/file_evaluator.rs", true)
+    }
+
+    #[test]
+    fn test_fail() -> Result<()> {
+        helper("src/evaluators/file_evaluator-dne.rs", false)
+    }
+}
